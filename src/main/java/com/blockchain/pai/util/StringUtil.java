05c4b343f012e18c423d41218cd200a98cdc4551
Pablo <pabloservile@gmail.com>
2019-06-18 11:27:39 -0300

    added dependency and string util methods

--- src/main/java/com/blockchain/pai/util/StringUtil.java
+++ src/main/java/com/blockchain/pai/util/StringUtil.java
@@ -63,6 +63,12 @@ public class StringUtil {
 		return Base64.getEncoder().encodeToString(key.getEncoded());
 	}
 
+	// Returns difficulty string target, to compare to hash. eg difficulty of 5 will
+	// return "00000"
+	public static String getDificultyString(int difficulty) {
+		return new String(new char[difficulty]).replace('\0', '0');
+	}
+
 //	Tacks in
 //	array of
 //	transactions and
